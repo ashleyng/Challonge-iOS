eb3db9fb0e90b7244585fa1ad5814285f9046b4c
Ashley Ng <ashleyng@users.noreply.github.com>
2018-12-31 12:33:36 -0600

    create live score viewmodel and attach undo button

--- Challonge/ViewControllers/PlayingMatchViewController.swift
+++ Challonge/ViewControllers/PlayingMatchViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import ChallongeNetworking
-import Crashlytics
 
 class PlayingMatchViewController: UIViewController {
     
@@ -20,22 +19,7 @@ class PlayingMatchViewController: UIViewController {
     @IBOutlet private var player1Label: UILabel!
     @IBOutlet private var player2Label: UILabel!
     
-    private var player1Score: Int = 0 {
-        didSet {
-            player1ScoreLabel.text = "\(player1Score)"
-        }
-    }
-    
-    private var player2Score: Int = 0 {
-        didSet {
-            player2ScoreLabel.text = "\(player2Score)"
-        }
-    }
-    
-    private let networking: ChallongeNetworking
-    private let match: Match
-    private let player1: Participant
-    private let player2: Participant
+    private let playingViewModel: PlayingMatchViewModel
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscape
@@ -46,10 +30,7 @@ class PlayingMatchViewController: UIViewController {
     }
     
     init(match: Match, player1: Participant, player2: Participant, networking: ChallongeNetworking) {
-        self.match = match
-        self.player1 = player1
-        self.player2 = player2
-        self.networking = networking
+        self.playingViewModel = PlayingMatchViewModel(match: match, player1: player1, player2: player2, challongeNetworking: networking)
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -73,42 +54,49 @@ class PlayingMatchViewController: UIViewController {
         pointPlayer1Button.roundEdges(withRadius: 10)
         pointPlayer2Button.roundEdges(withRadius: 10)
         undoButton.roundEdges(withRadius: 10)
+        undoButton.isEnabled = playingViewModel.canUndo()
         
-        player1Label.text = player1.name
-        player2Label.text = player2.name
+        player1Label.text = playingViewModel.getPlayerOneName()
+        player2Label.text = playingViewModel.getPlayerTwoName()
     }
     
     @objc
     private func submitScore(_ sender: Any) {
-        let winnerId = player1Score > player2Score ? player1.id : player2.id
-        let alert = createSubmitScoreAlert(winnerId: winnerId)
+        let alert = createSubmitScoreAlert()
         present(alert, animated: true, completion: nil)
     }
     
-    private func createSubmitScoreAlert(winnerId: Int) -> UIAlertController {
+    private func createSubmitScoreAlert() -> UIAlertController {
         let alert = UIAlertController(title: "Submit Score", message: "Are you sure you want to submit this score?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { [weak self] _ in
-            guard let `self` = self else { return }
-            self.networking.setWinnerForMatch(self.match.tournamentId, matchId: self.match.id, winnderId: winnerId, score: "\(self.player1Score)-\(self.player2Score)", completion: { match in
+            self?.playingViewModel.submitScore() {
                 DispatchQueue.main.async {
-                    self.navigationController?.popViewController(animated: true)
+                    self?.navigationController?.popViewController(animated: true)
                 }
-            }, onError: { error in
-                Answers.logCustomEvent(withName: "ErrorSubmittingScore", customAttributes: [
-                    "Error": error.localizedDescription
-                    ])
-            })
+            }
         }))
         return alert
     }
     
     @IBAction func pointPlayerOnePressed(_ sender: Any) {
-        player1Score += 1
+        let score = playingViewModel.increaseScoreForPlayerOne()
+        updateScore(scoreTuple: score)
     }
     
     @IBAction func pointPlayerTwoPressed(_ sender: Any) {
-        player2Score += 1
+        let score = playingViewModel.increaseScoreForPlayerTwo()
+        updateScore(scoreTuple: score)
     }
     
+    @IBAction func undoPressed(_ sender: Any) {
+        let score = playingViewModel.undoScore()
+        updateScore(scoreTuple: score)
+    }
+    
+    private func updateScore(scoreTuple: (player1: Int, player2: Int)) {
+        undoButton.isEnabled = playingViewModel.canUndo()
+        player1ScoreLabel.text = "\(scoreTuple.player1)"
+        player2ScoreLabel.text = "\(scoreTuple.player2)"
+    }
 }
