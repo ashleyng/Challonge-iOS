b39f9262e5c7c52967904b8edef7583117e32ba3
Ashley Ng <ashleyng@users.noreply.github.com>
2019-02-22 20:14:23 -0600

    add unit tests for view model

---
+++ ChallongeTests/PlayingMatchViewModelTests.swift
@@ -0,0 +1,97 @@
+//
+//  PlayingMatchViewModelTests.swift
+//  ChallongeTests
+//
+//  Created by Ashley Ng on 2/22/19.
+//  Copyright © 2019 AshleyNg. All rights reserved.
+//
+
+import XCTest
+// TODO: This should probably not be needed for unit tests
+import ChallongeNetworking
+@testable import Challonge
+
+class PlayingMatchViewModelTests: XCTestCase {
+    var viewModel: PlayingMatchViewModel!
+
+    override func setUp() {
+        let match = Match(id: 1234, player1Id: 23, player2Id: 24, state: .open, tournamentId: 4567, winnerId: nil, scoresCsv: nil, suggestedPlayOrder: nil, player1Votes: nil, player2Votes: nil)
+        let player1 = Participant(name: "Participant One", tournamentId: 4567, icon: nil, id: Participant.Id(main: 23, group: []))
+        let player2 = Participant(name: "Participant Two", tournamentId: 4567, icon: nil, id: Participant.Id(main: 24, group: []))
+        
+        viewModel = PlayingMatchViewModel(match: match, player1: player1, player2: player2, challongeNetworking: ChallongeNetworking(username: "username", apiKey: "apikey"))
+    }
+
+    func testGetPlayerNames() {
+        let actualPlayerNames = viewModel.getPlayerNames()
+        let expectedPlayerNames = (player1: "Participant One", player2: "Participant Two")
+        
+        XCTAssertEqual(actualPlayerNames.player1, expectedPlayerNames.player1)
+        XCTAssertEqual(actualPlayerNames.player2, expectedPlayerNames.player2)
+    }
+    
+    func testIncreasePlayerOneScore() {
+        let expectedPlayerOneScore = 2
+        viewModel.increaseScoreForPlayerOne()
+        viewModel.increaseScoreForPlayerOne()
+        
+        XCTAssertEqual(viewModel.getScore().player1, expectedPlayerOneScore)
+    }
+    
+    func testIncreasePlayerTwoScore() {
+        let expectedPlayerTwoScore = 1
+        viewModel.increaseScoreForPlayerTwo()
+        
+        XCTAssertEqual(viewModel.getScore().player2, expectedPlayerTwoScore)
+    }
+    
+    func testUndoScore() {
+        let expectedScoresBeforeUndo = (player1: 3, player2: 2)
+        let expectedScoresAfterUndo = (player1: 1, player2: 2)
+        viewModel.increaseScoreForPlayerOne()
+        viewModel.increaseScoreForPlayerTwo()
+        viewModel.increaseScoreForPlayerTwo()
+        viewModel.increaseScoreForPlayerOne()
+        viewModel.increaseScoreForPlayerOne()
+        
+        let actualScoresBeforeUndo = viewModel.getScore()
+        XCTAssertEqual(actualScoresBeforeUndo.player1, expectedScoresBeforeUndo.player1)
+        XCTAssertEqual(actualScoresBeforeUndo.player2, expectedScoresBeforeUndo.player2)
+        
+        viewModel.undoScore()
+        viewModel.undoScore()
+        
+        let actualScoresAfterUndos = viewModel.getScore()
+        XCTAssertEqual(actualScoresAfterUndos.player1, expectedScoresAfterUndo.player1)
+        XCTAssertEqual(actualScoresAfterUndos.player2, expectedScoresAfterUndo.player2)
+    }
+    
+    func testCanUndo() {
+        XCTAssertFalse(viewModel.canUndo())
+        viewModel.increaseScoreForPlayerTwo()
+        XCTAssertTrue(viewModel.canUndo())
+    }
+    
+    func testFlipUserNames() {
+        let expectedPlayerNames = (player1: "Participant Two", player2: "Participant One")
+        viewModel.flipUsers()
+        
+        let actualPlayerNames = viewModel.getPlayerNames()
+        XCTAssertEqual(actualPlayerNames.player1, expectedPlayerNames.player1)
+        XCTAssertEqual(actualPlayerNames.player2, expectedPlayerNames.player2)
+    }
+    
+    func testFlipUserScores() {
+        let expectedPlayerScores = (player1: 2, player2: 1)
+        viewModel.increaseScoreForPlayerOne()
+        viewModel.increaseScoreForPlayerTwo()
+        viewModel.increaseScoreForPlayerTwo()
+        
+        viewModel.flipUsers()
+        
+        let actualPlayerScores = viewModel.getScore()
+        XCTAssertEqual(actualPlayerScores.player1, expectedPlayerScores.player1)
+        XCTAssertEqual(actualPlayerScores.player2, expectedPlayerScores.player2)
+    }
+
+}
--- Podfile
+++ Podfile
@@ -1,14 +1,21 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '11.0'
+
+def shared_pods
+    pod 'ChallongeNetworking'
+#    pod 'ChallongeNetworking', :path => '../ChallongeNetworking'
+end
 
 target 'Challonge' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Challonge
-  pod 'ChallongeNetworking'
-#  pod 'ChallongeNetworking', :path => '../ChallongeNetworking'
+  shared_pods
   pod 'Fabric'
   pod 'Crashlytics'
   pod 'SnapKit'
+end
+
+target 'ChallongeTests' do
+    shared_pods
 end
